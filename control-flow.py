26f255e359ee886b18c6ef888630714aabc458e4
emilio girardi <emilio.g.girardi@gmail.com>
2021-12-24 19:21:45 +0100

    control flow

--- control-flow.py
+++ control-flow.py
@@ -11,3 +11,27 @@ elif number == 1:
     print("Single")
 else:
     print("More")
+
+
+# if statements and functions
+def modify_name(name):
+    if len(name) > 5:
+        return name.upper()
+    else:
+        return name.lower()
+
+
+new_name = modify_name("Luke")
+print(new_name)
+
+
+# nested if statements
+def num_info(num):
+    if num > 0:
+        print("Greater than zero")
+        if num > 10:
+            print("Also greater than ten")
+
+
+num_info(3)
+
