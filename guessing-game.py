7d5de6f6894e1c1880e2f9aaab9fc4a1ccfa8c52
emilio girardi <emilio.g.girardi@gmail.com>
2021-12-25 15:05:41 +0100

    guessing game using while loop

--- guessing-game.py
+++ guessing-game.py
@@ -1,15 +1,32 @@
 import random
 
+print(("*" * 30) + " GUESS THE SECRET NUMBER " + ("*" * 30))
+print("")
+
 # generate random number between 1 and 10
 secret_number = random.randint(1, 10)
 
 # keep track of user's guesses
-number_of_guesses = 0
+guess_count = 0
+guess_limit = 3
 
-# ask user to choose a number
-guess = input("Enter a number between 1 and 10: ")
+while guess_count < guess_limit:
 
-print(("*" * 100) + "GUESS THE SECRET NUMBER" + ("*" * 100))
-print("*" * 300)
-# while number_of_guesses < 3 and guess != secret_number:
+    # ask user to choose a number
+    guess = int(input("Enter a number between 1 and 10: "))
+    guess_count += 1
 
+    # check if user made the right guess
+    if guess == secret_number:
+        print("*" * 50)
+        print("CONGRATS! You've guessed the right number!")
+        print("*" * 50)
+    else:
+        if guess_count < guess_limit:
+            print("Wrong number!")
+            print("Try again!")
+
+print("")
+print("Sorry! You've run out of guesses.")
+print("")
+print(f"The secret number was: {secret_number}")
